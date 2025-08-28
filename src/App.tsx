c13c6e4f8ca77c51b3199b43fd332d70d69c8c31
rebhi walid <DigitifyDevTeam>
2025-08-28 09:09:35 +0100

    Fix scroll position issue: Add ScrollToTop component to automatically scroll to top on page navigation

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { LanguageProvider } from "@/contexts/LanguageContext";
+import ScrollToTop from "@/components/ui/ScrollToTop";
 import Index from "./pages/Index";
 import AircraftDetailing from "./pages/AircraftDetailing";
 import InteriorCleaning from "./pages/InteriorCleaning";
@@ -25,6 +26,7 @@ const App = () => (
         <Toaster />
         <Sonner />
         <BrowserRouter>
+          <ScrollToTop />
           <Routes>
             <Route path="/" element={<Index />} />
             <Route path="/nettoyage-exterieur" element={<AircraftDetailing />} />
